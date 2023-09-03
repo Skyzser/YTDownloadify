ea504f55a04ccc0b0a66e20f1035c6679d469d47
Boris Sky <bor2002isky@gmail.com>
2023-09-03 21:44:00 +0100

    Updated layout to include navbar/footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,10 @@
-import './globals.css';
 import type { Metadata } from 'next';
+
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+import Navbar from '@/components/navbar';
+import Footer from '@/components/footer';
+import './globals.css';
 
 export const metadata: Metadata = {
   title: 'YT2MP3',
@@ -14,7 +19,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className='ms-10 me-10 mt-5 mb-5 dark_BGPrimary dark_TextPrimary'>
+        <Navbar />
+        <div>{children}</div>
+        <Footer />
+      </body>
     </html>
   )
 }
