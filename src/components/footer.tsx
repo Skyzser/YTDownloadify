b93fe0b2cb41f03842a1d7b9fe807f27009076b7
Boris Sky <bor2002isky@gmail.com>
2023-09-03 21:43:35 +0100

    Finished simple navbar/footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <div className='flex flex-row justify-center'>
+            <a className='dark_Hover' href='https://github.com/Skyzser/YT2MP3' target='_blank'>GitHub</a>
+        </div>
+    )
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return (
+        <div className='flex flex-row justify-between'>
+            <div className='font-bold text-4xl'>YT2MP3</div>
+            <div className='text-3xl'>
+                <i className='bi bi-sun me-5 dark_Hover'></i>
+                <i className='bi bi-moon ms-5 dark_Hover'></i>
+            </div>
+        </div>   
+    )
+}
