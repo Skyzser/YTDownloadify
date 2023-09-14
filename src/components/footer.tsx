1a43b41263847a3e60e5df613e6c1bb4beb99a90
Boris Sky <bor2002isky@gmail.com>
2023-09-14 18:18:09 +0100

    Updated light mode styling and updated hover to be generalised

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <div className='flex flex-row justify-center items-center h-12 dark_BGSecondary'>
-            <a className='custom_Text dark_Hover' href='https://github.com/Skyzser/YTDownloadify' target='_blank'>GitHub</a>
+            <a className='custom_Text custom_Hover' href='https://github.com/Skyzser/YTDownloadify' target='_blank'>GitHub</a>
         </div>
     )
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,8 +3,8 @@ export default function Navbar() {
         <div className='flex flex-row justify-between items-center h-24 ps-10 pe-10 dark_BGSecondary'>
             <div className='custom_Title'>YTDownloadify</div>
             <div className='custom_Title flex flex-nowrap'>
-                <i className='bi bi-sun me-5 dark_Hover'></i>
-                <i className='bi bi-moon ms-5 dark_Hover'></i>
+                <i className='bi bi-sun me-5 custom_Hover'></i>
+                <i className='bi bi-moon ms-5 custom_Hover'></i>
             </div>
         </div>   
     )
