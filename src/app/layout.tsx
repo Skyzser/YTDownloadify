0a9d2ff07fb5de68fcb9c62f8adc252df9ec6f67
Boris Sky <bor2002isky@gmail.com>
2023-09-17 18:17:56 +0100

    Updated layout to be a server component rather than client, and now uses new provider for theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,27 @@
-"use client";
+import { Metadata } from 'next';
 
-import { useState } from 'react';
-import 'bootstrap-icons/font/bootstrap-icons.css'
-
+import Providers from '@/components/providers';
 import Navbar from '@/components/navbar';
 import Footer from '@/components/footer';
+
 import './globals.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+export const metadata: Metadata = {
+  title: 'YTDownloadify',
+  description: 'Downloads YouTube videos to MP3/MP4 files.',
+  keywords: 'youtube, mp3, mp4, converter',
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'dark');
-
-  const updateTheme = () => {
-    if (theme === 'dark') {
-      localStorage.setItem('theme', 'light');
-      setTheme('light');
-    } else {
-      localStorage.setItem('theme', 'dark');
-      setTheme('dark');
-    }
-  }
-
   return (
-    <html lang="en">
-      <head>
-        <title>YTDownloadify</title>
-        <meta name="description" content="Downloads YouTube videos to MP3/MP4 files." />
-        <meta name="keywords" content="youtube, mp3, mp4, converter" />
-        <meta name="author" content="Skyzser" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-      </head>
-      <body className={`flex flex-col h-screen ${theme}_BGPrimary ${theme}_Text`}>
-        <Navbar theme={theme} updateTheme={updateTheme} />
-        <div className='flex flex-1 overflow-auto'>{children}</div>
-        <Footer theme={theme} />
+    <html lang='en' className='dark' style={{ colorScheme: 'dark' }}>
+      <body className={`flex flex-col h-screen`}>
+        <Providers>
+          <Navbar />
+          <div className='flex flex-1 overflow-auto'>{children}</div>
+          <Footer />
+        </Providers>
       </body>
     </html>
   )
