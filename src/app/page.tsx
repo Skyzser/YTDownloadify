c404381865eef4db2be51da2e9e5a00a52b2d24c
Boris Sky <bor2002isky@gmail.com>
2023-09-21 20:32:30 +0100

    Created simple page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 'use client';
 
 import { useTheme } from 'next-themes';
+import SearchBox from '../components/Page/searchBox';
+import Content from '../components/Page/content';
 
 export default function Home() {
     const { theme } = useTheme();
     return (
-        <div className={`${theme}_BGPrimary w-full overflow-auto`}>
-            <div>Hello world</div>
+        <div className={`flex justify-center w-full pt-10 pb-10 ${theme}_BGPrimary`}>
+            <div className='flex flex-col h-full w-3/4'>
+                <div className='bg-red-500 flex justify-center items-center h-24'>
+                    <SearchBox />
+                </div>
+                <div className='bg-green-500 flex flex-1 justify-center overflow-auto pt-12'>
+                    <Content />
+                </div>
+            </div>
         </div>
     )
 }
